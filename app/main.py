c81f3b8e52a71df627ab29767fe3eace1557485f
Greshch <greshchook@gmail.com>
2024-11-20 19:08:13 +0200

    10 tests were passed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 class Person:
-    # write your code here
-    pass
+    people = []
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people.append({"name": name, "age": age})
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    persons = []
+    for obj in people:
+        person = Person(obj["name"], obj["age"])
+        attribute = "wife" if "wife" in obj else "husband"
+        value = obj[attribute]
+        person.__setattr__(attribute, value)
+        persons.append(person)
+    return persons
