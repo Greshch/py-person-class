705ef2432e4c822d770bfddd59fd0ad84f10a848
Greshch <greshchook@gmail.com>
2024-12-10 11:47:48 +0200

    all the tests were passed

--- app/main.py
+++ app/main.py
@@ -1,18 +1,27 @@
 class Person:
-    people = []
+    people = {}
+    # people = []
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people.append({"name": name, "age": age})
-
+        # Person.people.append({"name": name, "age": age})
+        Person.people[name] = self
 
 def create_person_list(people: list) -> list:
     persons = []
-    for obj in people:
-        person = Person(obj["name"], obj["age"])
-        attribute = "wife" if "wife" in obj else "husband"
-        value = obj[attribute]
-        person.__setattr__(attribute, value)
-        persons.append(person)
+    for person in people:
+        obj = Person(person["name"], person["age"])
+        partner = "wife" if "wife" in person else "husband"
+        obj.__setattr__(partner, person[partner])
+        persons.append(obj)
+
+    for person in persons:
+        partner = "wife" if hasattr(person, "wife") else "husband"
+        key = person.__getattribute__(partner)
+        if key is None:
+            person.__delattr__(partner)
+        else:
+            obj = Person.people[key]
+            person.__setattr__(partner, obj)
     return persons
